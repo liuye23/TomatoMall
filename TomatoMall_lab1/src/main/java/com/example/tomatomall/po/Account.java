608e96eca905a548b656e87c18ad10059d0cdf69
Zhang.JC <2733874993@qq.com>
2025-03-27 10:50:42 +0800

    m

--- TomatoMall_lab1/src/main/java/com/example/tomatomall/po/Account.java
+++ TomatoMall_lab1/src/main/java/com/example/tomatomall/po/Account.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.tomatomall.po;
 
 import lombok.Getter;
@@ -11,3 +12,24 @@ public class Account {
     private String username;
     private String password;
 }
+=======
+package com.example.tomatomall.po;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+    private String username;
+    private String password;
+    private String role;
+    private String email;
+    private String phone;
+    private String address;
+    private String realName;
+    private String idCard;
+}
+>>>>>>> b24d286 (create gitignore)
